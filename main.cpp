42b9d439b6ec829f374846f4d8598e83dc437401
Hyunmin Jung <jung@hyunmin.dev>
2021-03-06 18:52:59 +0900

    success code

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <stack>
 
 using namespace std;
 
+vector<int> solution(vector<int> input_vec) {
+  vector<int> output_vec(input_vec.size());
+  stack<int> stack;
+  for (int i = 0; i < input_vec.size(); i++) {
+    while (!stack.empty() && input_vec[stack.top()] > input_vec[i]) {
+      output_vec[stack.top()] = i - stack.top();
+      stack.pop();
+    }
+    stack.push(i);
+  }
+  while (!stack.empty()) {
+    output_vec[stack.top()] = input_vec.size() - stack.top() - 1;
+    stack.pop();
+  }
+  return output_vec;
+}
+
 int main() {
-  
+  vector<int> output_vec = solution({1, 2, 3, 4, 1});
+  for (const auto output : output_vec) {
+    cout << output << ", ";
+  }
   return 0;
 }
