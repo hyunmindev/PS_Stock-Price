203b91bdc54df67d1d6fbcb40b4b3f9b9b330fdf
Hyunmin Jung <jung@hyunmin.dev>
2021-03-08 23:47:22 +0900

    complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-stock-price
+# Printer Queue
+
+### strategy
+Stack
+
+# flowchart
+![flowchart](https://github.com/hyunmindev/PS_Stock-Price/blob/main/stock%20price-2.png)
--- main.cpp
+++ main.cpp
@@ -4,32 +4,7 @@
 
 using namespace std;
 
-vector<int> solution1(vector<int> input_vec) {
-  vector<int> output_vec(input_vec.size());
-  stack<int> stack;
-  stack.push(input_vec[0]);
-  for (int i = 1; i < input_vec.size(); ++i) {
-    if (input_vec[i] < stack.top()) {
-      int count = 1;
-      int j = i;
-      while (!stack.empty() && input_vec[i] < stack.top()) {
-        stack.pop();
-        if (output_vec[--j] == 0) {
-          output_vec[j] = count++;
-        }
-      }
-    }
-    stack.push(input_vec[i]);
-  }
-  for (int i = 0; i < output_vec.size(); ++i) {
-    if (output_vec[i] == 0) {
-      output_vec[i] = output_vec.size() - i - 1;
-    }
-  }
-  return output_vec;
-}
-
-vector<int> solution2(vector<int> input_vec) {
+vector<int> solution(vector<int> input_vec) {
   vector<int> output_vec(input_vec.size());
   stack<int> stack;
   for (int i = 0; i < input_vec.size(); i++) {
@@ -47,14 +22,9 @@ vector<int> solution2(vector<int> input_vec) {
 }
 
 int main() {
-  vector<int> input_vec{1, 2, 3, 2, 3, 1};
-  vector<int> output_vec1 = solution1(input_vec);
-  vector<int> output_vec2 = solution2(input_vec);
-  for (const auto output : output_vec1) {
-    cout << output << ", ";
-  }
-  cout << endl;
-  for (const auto output : output_vec2) {
+  vector<int> input_vec{2, 3, 5, 4, 1};
+  vector<int> output_vec = solution(input_vec);
+  for (const auto output : output_vec) {
     cout << output << ", ";
   }
   return 0;
